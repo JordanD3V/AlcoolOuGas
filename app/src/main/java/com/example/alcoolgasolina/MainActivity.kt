f0a14d9cc2d32a63820dbcdbfd93e89c2410e0c1
Jordan Henrique <jordandev586@gmail.com>
2024-09-07 17:17:39 -0300

    Lógica App Alcool ou Gasolina

--- app/src/main/java/com/example/alcoolgasolina/MainActivity.kt
+++ app/src/main/java/com/example/alcoolgasolina/MainActivity.kt
@@ -1,12 +1,26 @@
 package com.example.alcoolgasolina
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var textInputAlcool: TextInputLayout
+    private lateinit var editInputAlcool: TextInputEditText
+
+    private lateinit var textInputGasolina: TextInputLayout
+    private lateinit var editInputGasolina: TextInputEditText
+
+    private lateinit var buttonCalcular: Button
+    private lateinit var txtResultado: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +30,56 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        inicializarComponentesInterface()
+
+        buttonCalcular.setOnClickListener {
+            calcularMelhorPreço()
+        }
     }
+
+    private fun inicializarComponentesInterface() {
+        textInputAlcool = findViewById(R.id.txt_input_alcool)
+        textInputGasolina = findViewById(R.id.txt_input_gasolina)
+
+        editInputAlcool = findViewById(R.id.edit_txt_alcool)
+        editInputGasolina = findViewById(R.id.edit_txt_gasolina)
+
+        buttonCalcular = findViewById(R.id.btn_calcular)
+        txtResultado = findViewById(R.id.txt_resultado)
+    }
+
+    private fun validarCampos(precoAlcool: String, precoGasolina: String): Boolean {
+        textInputAlcool.error = null
+        textInputGasolina.error = null
+
+        if (precoAlcool.isEmpty()) {
+            textInputAlcool.error = "Digite o preço do álcool"
+            return false
+        } else if (precoGasolina.isEmpty()) {
+            textInputGasolina.error = "Digite o preço da gasolina"
+            return false
+        }
+
+        return true
+    }
+
+    private fun calcularMelhorPreço() {
+
+        val precoAlcool = editInputAlcool.toString()
+        val precoGasolina = editInputGasolina.toString()
+
+
+        val precoAlcoolNumber = precoAlcool.toDouble()
+        val precoGasolinaNumber = precoGasolina.toDouble()
+        val resultado = (precoAlcoolNumber / precoGasolinaNumber)
+
+        if (resultado >= 0.7) {
+            txtResultado.text = "É melhor abastecer com gasolina"
+        } else {
+            txtResultado.text = "É melhor abastecer com álcool"
+        }
+    }
+
+
 }
